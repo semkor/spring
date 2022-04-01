da99008ee7cf92b0b36fd67e31222bf90fc38ec1
semkor <selsasss@gmail.com>
2022-04-01 21:06:06 +0300

    new

--- src/main/java/com/MySpringMvcServlet.java
+++ src/main/java/com/MySpringMvcServlet.java
@@ -1,20 +1,22 @@
 package com;
 
-import com.lesson4.hw2.MyConfig;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
-
-//этот класс заменяет  web.xml
-public class MySpringMvcServlet extends AbstractAnnotationConfigDispatcherServletInitializer {
+public class MySpringMvcServlet extends AbstractAnnotationConfigDispatcherServletInitializer  {
 
     @Override
     protected Class<?>[] getRootConfigClasses() {
         return null;
     }
 
+//    @Override
+//    protected Class<?>[] getServletConfigClasses() {
+//        return new Class[] {MyConfig.class};
+//    }
+
     @Override
     protected Class<?>[] getServletConfigClasses() {
-        return new Class[] {AppConfig.class};  //для lesson4.hw2 правильно писать {MyConfig.class}
+        return new Class[] {AppConfig.class};
     }
 
     @Override
