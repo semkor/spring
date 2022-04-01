9760b62aecd9849491ae0c64a9b27533804c00be
semkor <selsasss@gmail.com>
2022-04-01 21:05:43 +0300

    new

--- src/main/java/com/lesson2/hw2/ItemController.java
+++ src/main/java/com/lesson2/hw2/ItemController.java
@@ -1,64 +1,39 @@
 package com.lesson2.hw2;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import java.util.Date;
 
-@Controller
+@RestController
 @RequestMapping("/first")
 public class ItemController {
-
-    @Autowired
     private ItemService itemService;
 
-    @RequestMapping(method= RequestMethod.POST, value = "/save", produces = {MediaType.ALL_VALUE})
-    public @ResponseBody String save(@RequestParam("name") String name,
-                                     @RequestParam("dateCreated") Date dateCreated,
-                                     @RequestParam("lastUpdatedDate") Date lastUpdatedDate,
-                                     @RequestParam("description") String description){
-        System.out.println("Получили параметр");
-        Item item=new Item();
-            item.setNames(name);
-            item.setDateCreated(dateCreated);
-            item.setLastUpdatedDate(lastUpdatedDate);
-            item.setDescription(description);
-            itemService.save(item);
-        return "Save - ok";
+    @Autowired
+    public ItemController(ItemService itemService) {
+        this.itemService = itemService;
     }
 
-
-    @RequestMapping(method= RequestMethod.PUT, value = "/update", produces = {MediaType.ALL_VALUE})
-    public @ResponseBody String update(@RequestParam("id") long id, @RequestParam("name") String name,
-                                       @RequestParam("dateCreated") Date dateCreated,
-                                       @RequestParam("lastUpdatedDate") Date lastUpdatedDate,
-                                       @RequestParam("description") String description){
-        System.out.println("Получили параметр");
-        Item item=new Item();
-            item.setId(id);
-            item.setNames(name);
-            item.setDateCreated(dateCreated);
-            item.setLastUpdatedDate(lastUpdatedDate);
-            item.setDescription(description);
-        itemService.update(item);
-        return "Update - ok";
-    }
-
-    @RequestMapping(method= RequestMethod.GET, value = "/find", produces = "text/plain")
-    public @ResponseBody String findById(@RequestParam(value = "param")long id){
-        System.out.println("Получили параметр");
+    @GetMapping(value = "/find")
+    public String findById(@RequestParam(value = "param") long id){
         itemService.findById(id);
         return "findBy-OK";
     }
 
-    @RequestMapping(method= RequestMethod.DELETE, value = "/delete", produces = "text/plain")
-    public @ResponseBody String delete(@RequestParam(value = "param")long id){
-        System.out.println("Получили параметр");
+    @DeleteMapping(value = "/delete/{id}")
+    public String delete(@PathVariable ("id") long id){
         itemService.delete(id);
         return "Delete-OK";
     }
+
+    @PostMapping(value = "/save")
+    public String create(@RequestBody Item item){
+        itemService.save(item);
+        return "Save - ok";
+    }
+
+    @PutMapping(value = "/update/{id}")
+    public String update (@PathVariable (name = "id") long id, @RequestBody Item item){
+        itemService.update(item);
+        return "Update - ok";
+    }
 }
-
-
-
