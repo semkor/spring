adddd868b7c165370f36a09cefb474cf495dacb0
semkor <selsasss@gmail.com>
2022-04-01 21:03:14 +0300

    new

--- src/main/java/com/lesson2/hw1/Controllers.java
+++ src/main/java/com/lesson2/hw1/Controllers.java
@@ -10,26 +10,19 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class Controllers {
     @Autowired
     private Service service;
+
     @Autowired
     private Step step;
+
     @Autowired
     private Route route;
 
     @RequestMapping(method= RequestMethod.GET, value = "/result", produces = "text/plain")
     public @ResponseBody String callByBeans(){
-        System.out.println("Get класса Service");
-            System.out.println(service.getId());
-            System.out.println(service.getName());
-            System.out.println(service.getParamsToCall());
-        System.out.println("Get класса Step");
-            System.out.println(step.getId());
-            System.out.println(step.getServiceFrom());
-            System.out.println(step.getServiceTo());
-            System.out.println(step.getParamServiceFrom());
-            System.out.println(step.getParamsServiceTo());
-        System.out.println("Get класса Route");
-            System.out.println(route.getId());
-            System.out.println(route.getSteps());
+        System.out.println("Get класса Service: " + " " + service.getId() + " " + service.getName() + " " + service.getParamsToCall());
+        System.out.println("Get класса Step: " + " " + step.getId() + " " + step.getServiceFrom() + " " + step.getServiceTo()
+                + " " + step.getParamServiceFrom()  + " " + step.getParamsServiceTo());
+        System.out.println("Get класса Route: "  + " " + route.getId()  + " " + route.getSteps());
         return "ok";
     }
 }
